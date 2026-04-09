90ca9ce97cd7a8f29c050904360a818f1b5020d7
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-09 18:46:58 +0530

    Add output

--- junk/var_lab.py
+++ junk/var_lab.py
@@ -1,4 +1,4 @@
 # Variable in Python = It is like a container that holds data. It is used to store values that can be changed during the execution of a program.
 
 github_UserName = "Prakharr3act"
-print(github_UserName)
+print(github_UserName) # Output: Prakharr3act
