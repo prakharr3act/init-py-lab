13b3ba868b39add3b71eebcc011e7dc3613c695c
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-09 19:17:51 +0530

    Add if_else_lab.py

---
+++ junk/if_else_lab.py
@@ -0,0 +1,10 @@
+
+# if and else in python
+
+is_User_Online = False #can ve True or False
+player_UserName = "Prakharr3act"
+
+if is_User_Online:
+   print(f"Welocome {player_UserName} to the game!") # Output: Welcome Prakharr3act to the game!
+else:
+   print(f"Sorry {player_UserName}, you need to be online to play the game.")
