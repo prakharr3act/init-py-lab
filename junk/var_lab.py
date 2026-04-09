764b0f4e9e46fdd6f76b7bd7ffc3e841d63a634c
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-09 18:46:29 +0530

    Add github_UserName

--- junk/var_lab.py
+++ junk/var_lab.py
@@ -1,2 +1,4 @@
 # Variable in Python = It is like a container that holds data. It is used to store values that can be changed during the execution of a program.
 
+github_UserName = "Prakharr3act"
+print(github_UserName)
