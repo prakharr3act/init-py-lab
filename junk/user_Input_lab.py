71a36e5b6b9de8bded9b93d5007adc3875164b06
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-10 17:14:01 +0530

    add input( )

--- junk/user_Input_lab.py
+++ junk/user_Input_lab.py
@@ -6,3 +6,7 @@ print("Hello, " + name + "!")
 
 id = input("Can you enter your id for verification? ")
 print("Your id is: " + id)
+
+pasword = input("Please enter your password: ")
+print("Your password is: " + pasword + "You have been successfully logged in.")
+print("Welcome to the system, " + name + "! Your id is " + id + " and your password is " + pasword + "." + "You can now survillence the system and access your account.")
