65fad9300df9facd3641cb2cce55d77375603146
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-10 17:37:55 +0530

    add input for cart

---
+++ exercise/shopping_Cart_Program_lab.py
@@ -0,0 +1,5 @@
+# Exercise 2 Shopping Cart Program
+
+item = input("Enter the item you want to purchase: ")
+quantity = int(input("Enter the quantity: "))
+price = float(input("Enter the price of the item: "))
