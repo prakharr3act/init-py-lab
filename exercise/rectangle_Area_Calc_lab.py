c7c81db32f0bc85b440929316843cabc6bf5e08e
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-10 17:25:15 +0530

    change length to float

--- exercise/rectangle_Area_Calc_lab.py
+++ exercise/rectangle_Area_Calc_lab.py
@@ -1,6 +1,6 @@
 # Reactangle Area Calc
 
-length = input("Enter the Length of the reactangle: ")
-width = input("Enter the Width of the reactangle: ")
+length = float(input("Enter the Length of the reactangle: "))
+width = float(input("Enter the Width of the reactangle: "))
 area = length * width
 print("The area of the reactangle is: " + str(area))
