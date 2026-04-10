3943c8995836f3c6f1d534c51614f7015a32e83b
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-10 17:02:58 +0530

    add input to get user data

--- junk/user_Input_lab.py
+++ junk/user_Input_lab.py
@@ -1,1 +1,5 @@
-# input() function prompts the user to enter data and return the data in string format. 
+# input()    function prompts the user to enter data and
+#                 return the data in string format. 
+
+name = input("What is your name? ")
+print("Hello, " + name + "!")
