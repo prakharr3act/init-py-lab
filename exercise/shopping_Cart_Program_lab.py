84508c4e3f0cc717de30a3790caea01b654d277e
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-10 17:41:07 +0530

    print total cost

--- exercise/shopping_Cart_Program_lab.py
+++ exercise/shopping_Cart_Program_lab.py
@@ -4,4 +4,4 @@ item = input("Enter the item you want to purchase: ")
 quantity = int(input("Enter the quantity: "))
 price = float(input("Enter the price of the item: "))
 total_Cost = quantity * price
-print("The total cost of your purchase is: $", {total_Cost})
+print(f"The total cost of your purchase is: ${total_Cost:.2f}")
