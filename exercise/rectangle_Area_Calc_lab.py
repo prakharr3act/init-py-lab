8fc07d1ab20cb183a79a5d488bf00c3c0d62e79a
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-10 17:24:41 +0530

    print out area

--- exercise/rectangle_Area_Calc_lab.py
+++ exercise/rectangle_Area_Calc_lab.py
@@ -2,3 +2,5 @@
 
 length = input("Enter the Length of the reactangle: ")
 width = input("Enter the Width of the reactangle: ")
+area = length * width
+print("The area of the reactangle is: " + str(area))
