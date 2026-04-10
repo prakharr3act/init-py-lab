60fe4c59fa09127a2a7e3f1642cc7c5ea6f47272
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-10 11:48:44 +0530

    Add variables

--- junk/type_Casting.lab.py
+++ junk/type_Casting.lab.py
@@ -1,1 +1,9 @@
 # TypeCasting is the process of converting a variable from one type to another. In Python, you can use built-in functions to perform type casting. (str(), int(), float(), etc.)
+
+# Example of type casting:
+Name = "John"
+Age = 30
+Is_Teacher = True
+gpa = 3.5
+
+# Converting variables to different types
