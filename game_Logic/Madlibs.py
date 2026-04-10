b10cff009728cb623e9cd437c3bb14668c61303c
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-10 17:51:57 +0530

    add rules

--- game_Logic/Madlibs.py
+++ game_Logic/Madlibs.py
@@ -1,5 +1,6 @@
 # Madlibs Game It is a word filling game. 
-# where the user is prompted to enter different types of words (nouns, verbs, adjectives, etc.) and then those words are inserted into a pre-written story template to create a funny or interesting story.
+# where the user is prompted to enter different types of words (nouns, verbs, adjectives, etc.) 
+# Those words are inserted into a pre-written story template to create a funny or interesting story.
 
 print("Welcome to the Madlibs Game!")
 print("Please enter the following words:")
@@ -8,6 +9,8 @@ adjective = input("Enter an adjective: ")
 noun = input("Enter a noun: ")
 verb = input("Enter a verb: ")
 adverb = input("Enter an adverb: ")
+
+# its better not to use profanity in the madlibs game as it is meant to be fun and suitable for all ages.
 
 story = f"The {adjective} {noun} {verb} {adverb}."
 print(story)
