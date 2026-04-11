6883eab11e29a8532a90e69b939a54b4d7c2019c
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-11 21:28:02 +0530

    add round fnc

--- junk/arithmetic_Math_lab.py
+++ junk/arithmetic_Math_lab.py
@@ -2,3 +2,10 @@ no_Of_Repositories = 30
 no_Of_Repositories = no_Of_Repositories + 10
 # no_Of_Repositories += 10         This and above one are same
 print(no_Of_Repositories)
+
+
+# Round( )function
+y = 12
+z = 21.032
+print(round(y)) # output: 12
+print(round(z)) # output: 21
