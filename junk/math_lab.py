80965487143eea66f04a3b13e293f3a749e1d7f9
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-12 14:25:33 +0530

    add ceil fnc

--- junk/math_lab.py
+++ junk/math_lab.py
@@ -3,3 +3,5 @@ import math
 print(math.pi)
 print(math.e) # e is the exponential constant, approximately equal to 2.71828
 print(math.sqrt(16)) # square root of 16 is 4
+print(math.ceil(3.7)) # ceiling of 3.7 is 4
+# ceil returns the smallest integer greater than or equal to the given number
