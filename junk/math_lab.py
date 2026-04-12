fa7d9ad890850d6d478fd4490cee99cf989bd84a
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-12 14:26:40 +0530

    add floor fnc

--- junk/math_lab.py
+++ junk/math_lab.py
@@ -4,4 +4,6 @@ print(math.pi)
 print(math.e) # e is the exponential constant, approximately equal to 2.71828
 print(math.sqrt(16)) # square root of 16 is 4
 print(math.ceil(3.7)) # ceiling of 3.7 is 4
-# ceil returns the smallest integer greater than or equal to the given number
+# ceil returns the smallest integer greater than or equal to the given number || round up
+print(math.floor(3.7)) # floor of 3.7 is 3
+# floor returns the largest integer less than or equal to the given number || round down
