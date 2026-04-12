8a34ef59dda912bceabf1306565444ba34ee6168
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-12 14:38:12 +0530

    round the area to two digits

--- exercise/area_Circle_lab.py
+++ exercise/area_Circle_lab.py
@@ -1,4 +1,4 @@
 import math
 radius = float(input("Enter the radius of the circle: "))
 area = math.pi * pow(radius, 2)
-print(f"The area of the circle with radius {radius} is: {area}cm²")
+print(f"The area of the circle with radius {radius} is: {round(area,2)}cm²")
