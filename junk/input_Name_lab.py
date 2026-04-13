6fca595a7ec4deb341366f0b999827959504d56c
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-13 22:12:24 +0530

    add else

--- junk/input_Name_lab.py
+++ junk/input_Name_lab.py
@@ -2,4 +2,4 @@ name = input("What is your name? ")
 if name == '':
   print("you don't have to be smart, just enter your name!!")
 else:
-  print()
+  print(f"Hello {name}, welcome to the world of programming!!")
