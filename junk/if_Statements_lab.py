bd87148540b65333c8603c42335ff71d31805cab
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-13 21:48:02 +0530

    add elif

--- junk/if_Statements_lab.py
+++ junk/if_Statements_lab.py
@@ -5,5 +5,7 @@ no_Of_Contributions = int(input("How many contributions have you made? "))
 if no_Of_Contributions > 80:
     print("You are a top contributor")
     # elif (else if in other languages) = do something if the previous conditions were false and this condition is true
+elif no_Of_Contributions > 50:
+    print("You are a good contributor")
 else:
     print("You need to contribute more")
