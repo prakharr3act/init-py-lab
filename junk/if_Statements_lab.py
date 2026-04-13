356ee1c9e68c6d37f45b25c020b283b0bbe88bc2
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-13 21:59:09 +0530

    add else

--- junk/if_Statements_lab.py
+++ junk/if_Statements_lab.py
@@ -15,5 +15,7 @@ else:
 response =  input("What is your favorite programming language? ")
 if response == 'python':
     print("Python is a great language")
-    elif response == 'java':
-    print("Java is a great language")
+    if response == 'java':
+        print("Java is a great language")
+    else:
+        print("That's a good language too!")
