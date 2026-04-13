e6cee38c4274d428acdeded8910eb4f5ecaf10dd
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-13 22:12:59 +0530

    nest input

--- junk/input_Name_lab.py
+++ junk/input_Name_lab.py
@@ -1,5 +1,6 @@
 name = input("What is your name? ")
 if name == '':
   print("you don't have to be smart, just enter your name!!")
+  name = input("What is your name? ")
 else:
   print(f"Hello {name}, welcome to the world of programming!!")
