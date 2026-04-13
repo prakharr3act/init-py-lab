9b51fa5286ca20bd9c088b3e05419f05729da480
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-13 21:46:10 +0530

    add else

--- junk/if_Statements_lab.py
+++ junk/if_Statements_lab.py
@@ -1,6 +1,8 @@
 # if = do something if a condition is true
 # else = do something if the condition is false
 
-if no_Of_Contributions > 100:
+
+if no_Of_Contributions > 80:
     print("You are a top contributor")
-    
+else:
+    print("You need to contribute more")
