1380f927c7f141244f4295e7ab18d0fa33a7ab6f
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-13 22:01:46 +0530

    add response

---
+++ junk/if_Statementss_lab.py
@@ -0,0 +1,7 @@
+response =  input("What is your favorite programming language? ")
+if response == 'python':
+    print("Python is a great language")
+elif response == 'java':
+    print("Java is a great language")
+else:
+    print("That's a good language too!")
