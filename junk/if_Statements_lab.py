c09a51d432e498fb8a113b5f6cced0d79e2040e5
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-13 22:01:19 +0530

    add file

--- junk/if_Statements_lab.py
+++ junk/if_Statements_lab.py
@@ -12,10 +12,3 @@ elif no_Of_Contributions > 20:
 else:
     print("You have not contributed enough")
 
-response =  input("What is your favorite programming language? ")
-if response == 'python':
-    print("Python is a great language")
-elif response == 'java':
-    print("Java is a great language")
-else:
-    print("That's a good language too!")
