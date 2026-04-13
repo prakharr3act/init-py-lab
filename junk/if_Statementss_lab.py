317e02361dde9ced613acf9b1fded841a1837d64
Prakharr3act <prakhar.codes433@gmail.com>
2026-04-13 22:08:36 +0530

    add note

--- junk/if_Statementss_lab.py
+++ junk/if_Statementss_lab.py
@@ -11,3 +11,5 @@ if request == 'error':
     print("Enter your error id here")
 else:
     print("Your request is being processed")
+
+    # the code above is just written while learning and it's not meant to be used in production (no one gonna use it). It's just for learning purposes.
